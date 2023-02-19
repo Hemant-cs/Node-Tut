5658db4e65f1dfe10ad8df13c3358c0c20527b9b
Hemant-cs <hemantchaurasia555@gmail.com>
2023-02-19 14:23:51 +0530

    added new lines

--- BasicAPICreation.js
+++ BasicAPICreation.js
@@ -2,7 +2,9 @@ const HTTP = require('http');
 const data = require('./Data');
 
 HTTP.createServer((req,resp)=>{
+
     resp.writeHead(200,{"Content-Type":"application\json"})
     resp.write(JSON.stringify(data))
     resp.end()
+    
 }).listen('3000')
